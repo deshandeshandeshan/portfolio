705cc728a3eb65d437a12c20c58973204f7bee04
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-09-04 17:18:12 +1200

    Added content and links to Home and About components. Added an Images folder and imported an image into about page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <main className="home">
-      <h1>Home</h1>
+      <div className="home-main">
+        <h3>Web Developer & Designer</h3>
+        <Link to="/About">
+          <h3>About</h3>
+        </Link>
+        <h1>Deshan Mclachlan</h1>
+      </div>
+      <div className="home-projects"></div>
     </main>
   );
 };
