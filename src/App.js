68307097b683597ade3e3d0bb7970c75a1479ae4
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-10-02 17:39:02 +1300

    Installed framer motion packages and edited routing configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./components/Home";
 import About from "./components/About";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<Home />} />
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </ScrollToTop>
