0526900e014828564151b5111679a58a7d55dc11
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-12-16 18:27:44 +1300

    added a state to track project data, used an array find method to find a particular project by id.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,50 +1,61 @@
 import { FaLongArrowAltLeft } from "react-icons/fa";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import projects from "../projects-data/projectsdata";
 
-const Project = ({
-  name,
-  images,
-  yearCompleted,
-  description,
-  role,
-  credits,
-  visitLink,
-  visit,
-}) => {
+const Project = () => {
   const { id } = useParams();
   console.log(id);
+
+  const [project, setProject] = useState(projects);
+
+  const filterProjects = (id) => {
+    const filteredProject = projects.find((project) => project.id !== id);
+    setProject(filteredProject);
+  };
+
+  useEffect(() => {
+    filterProjects(id);
+  }, [id]);
+
+  console.log(project);
+
   return (
     <div>
       <p className="project-back-button project-link icon-center">
         <FaLongArrowAltLeft /> &nbsp;Back
       </p>
       <h2 className="project-overlay-title project-padding">
-        {name} <small>{yearCompleted}</small>
+        {project.name} <small>{project.yearCompleted}</small>
       </h2>
       <p className="project-overlay-description project-padding">
-        {description}
+        {project.description}
       </p>
-      <p className="project-overlay-role project-padding">Services: {role}</p>
-      <p className="project-overlay-credits project-padding">{credits}</p>
+      <p className="project-overlay-role project-padding">
+        Services: {project.role}
+      </p>
+      <p className="project-overlay-credits project-padding">
+        {project.credits}
+      </p>
       <a
         className="project-overlay-visit project-padding icon-center"
-        href={visitLink}
+        href={project.visitLink}
       >
-        {visit}&nbsp; <FaLongArrowAltRight />
+        {project.visit}&nbsp; <FaLongArrowAltRight />
       </a>
-      {/* <div className="project-overlay-images project-padding-top project-overlay-image-grid">
-        {images.map((projectImage) => {
+      <div className="project-overlay-images project-padding-top project-overlay-image-grid">
+        {project.images.map((projectImage) => {
           return (
             <img
               className={`project-overlay-image ${projectImage.class}`}
               src={`${projectImage.url}`}
-              alt={name}
+              alt={project.name}
               key={projectImage.key}
             />
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 };
